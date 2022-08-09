afc60dcd8f7ce7d7ad785ee12a402596b8f1aa22
hc_zhang <hc_zhang@trip.com>
2022-08-09 16:56:21 +0800

    add Panosen.Generation.Extension.csproj

--- 0 - Build-Publish-Move.bat
+++ 0 - Build-Publish-Move.bat
@@ -5,5 +5,6 @@ dotnet restore
 dotnet build --no-restore -c Release
 
 move /Y Panosen.Generation\bin\Release\Panosen.Generation.*.nupkg D:\LocalSavoryNuget\
+move /Y Panosen.Generation.Extension\bin\Release\Panosen.Generation.Extension.*.nupkg D:\LocalSavoryNuget\
 
 pause
---
+++ Panosen.Generation.Extension/PackageExtension.cs
@@ -0,0 +1,56 @@
+﻿using Panosen.Toolkit;
+using System.IO;
+using System.Text;
+
+namespace Panosen.Generation
+{
+    /// <summary>
+    /// Package Extension
+    /// </summary>
+    public static class PackageExtension
+    {
+
+        /// <summary>
+        /// 写入到文件夹
+        /// </summary>
+        public static void WriteTo(this Package package, string output)
+        {
+            WriteTo(package, output, Encoding.UTF8);
+        }
+
+        /// <summary>
+        /// 写入到文件夹
+        /// </summary>
+        public static void WriteTo(this Package package, string output, Encoding encoding)
+        {
+            foreach (var item in package.Files)
+            {
+                var path = Path.Combine(output, item.FilePath);
+
+                var fileDirectory = Path.GetDirectoryName(path);
+                if (!Directory.Exists(fileDirectory))
+                {
+                    Directory.CreateDirectory(fileDirectory);
+                }
+
+                if (item is Panosen.Generation.PlainFile)
+                {
+                    if (File.Exists(path) && Hash.SHA256HEX(File.ReadAllBytes(path)) == Hash.SHA256HEX(encoding.GetBytes(((PlainFile)item).Content)))
+                    {
+                        continue;
+                    }
+                    File.WriteAllText(path, ((PlainFile)item).Content, encoding);
+                }
+
+                if (item is BytesFile)
+                {
+                    if (File.Exists(path) && Hash.SHA256HEX(File.ReadAllBytes(path)) == Hash.SHA256HEX(((BytesFile)item).Bytes))
+                    {
+                        continue;
+                    }
+                    File.WriteAllBytes(path, ((Panosen.Generation.BytesFile)item).Bytes);
+                }
+            }
+        }
+    }
+}
